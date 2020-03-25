0dfbab4395788b792cad5de690b564f9cc12d811
Pawel <wispawelwis38@gmail.com>
2020-03-26 00:54:45 +0100

    Solved task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,37 @@
 /* Here goes your JS code */
 const clickme_btn = document.getElementById("show-popup-form");
 const close_btn = document.getElementsByClassName("close-btn")[0];
+const popup = document.getElementsByClassName("popup")[0];
 
 clickme_btn.addEventListener("click", () => {
-  document.getElementsByClassName("popup")[0].style.display = "block";
+  popup.style.display = "block";
 });
 
 close_btn.addEventListener("click", () => {
-  document.getElementsByClassName("popup")[0].style.display = "none";
+  popup.style.display = "none";
 });
+
+function validate_form() {
+  const email = document.forms["login_form"]["email"].value;
+  const password = document.forms["login_form"]["password"].value;
+  const ischecked = document.forms["login_form"]["checkbox"].checked;
+
+  if (email === "") {
+    alert("Please enter your email address");
+    return false;
+  } else if (!/@/.test(email)) {
+    alert("Please enter a proper email address");
+    return false;
+  } else if (password === "") {
+    alert("Please enter your password");
+    return false;
+  } else if (password.length < 8) {
+    alert("Your password must contain at least 8 characters");
+    return false;
+  } else if (!ischecked) {
+    alert("Please accept the terms and conditions");
+    return false;
+  } else {
+    return true;
+  }
+}
