33f6bc3ea077fb9de0f3dc618a87d3bfd8a5d2f6
Pawel <wispawelwis38@gmail.com>
2020-03-25 23:38:21 +0100

    Solved task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 /* Here goes your JS code */
+const clickme_btn = document.getElementById("show-popup-form");
+const close_btn = document.getElementsByClassName("close-btn")[0];
+
+clickme_btn.addEventListener("click", () => {
+  document.getElementsByClassName("popup")[0].style.display = "block";
+});
+
+close_btn.addEventListener("click", () => {
+  document.getElementsByClassName("popup")[0].style.display = "none";
+});
